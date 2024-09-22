c7fd6d1793ebff31f8f46dd18a307e5ed5e936d4
Gabriel Penteado <gabripenteado@gmail.com>
2024-09-22 00:10:26 -0300

    update baseURL in axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   //   baseURL: "https://dentalprocs-server.onrender.com",
   //   baseURL: "https://dentalprocs-server.cyclic.app",
-  baseURL: "http://localhost:3333",
+  baseURL: "https://dentalprocs-server.vercel.app",
+  //   baseURL: "http://localhost:3333",
 });
